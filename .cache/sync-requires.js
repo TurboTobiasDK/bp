5dec5699f4cb16bdab6bb3a4220a4eb74ba3a8b1
TurboTobiasDK <mig@turbotobias.dk>
2020-06-22 00:40:33 +0200

    Third commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,25 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\pages\\index.js"))),
+  "component---src-pages-kontakt-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\pages\\kontakt.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\pages\\page-2.js"))),
+  "component---src-pages-ydelser-andet-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\pages\\ydelser\\andet.js"))),
+  "component---src-pages-ydelser-auktionsskoede-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\pages\\ydelser\\auktionsskoede.js"))),
+  "component---src-pages-ydelser-berigtigelse-af-bolighandel-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\pages\\ydelser\\berigtigelse-af-bolighandel.js"))),
+  "component---src-pages-ydelser-koeber-gennemgang-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\pages\\ydelser\\koeber-gennemgang.js"))),
+  "component---src-pages-ydelser-koeberpakke-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\pages\\ydelser\\koeberpakke.js"))),
+  "component---src-pages-ydelser-selvsalg-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\pages\\ydelser\\selvsalg.js"))),
+  "component---src-pages-ydelser-skifteretsattest-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\pages\\ydelser\\skifteretsattest.js"))),
+  "component---src-pages-ydelser-skilsmisseskoede-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\pages\\ydelser\\skilsmisseskoede.js"))),
+  "component---src-pages-ydelser-tinglysning-af-skoede-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\pages\\ydelser\\tinglysning-af-skoede.js"))),
+  "component---src-templates-post-blog-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\templates\\post\\blog.js"))),
+  "component---src-templates-post-index-js": hot(preferDefault(require("C:\\Users\\mig\\Desktop\\Gatsby\\bolig-partner\\src\\templates\\post\\index.js")))
+}
+
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,82 +1,11 @@
 /* --------- Programatically Create Blog posts --------- */
+const createPosts = require("./create/createPosts")
 
-const _ = require(`lodash`)
-const Promise = require(`bluebird`)
-const path = require(`path`)
-const slash = require(`slash`)
-
-// Implement the Gatsby API “createPages”. This is
-// called after the Gatsby bootstrap is finished so you have
-// access to any information necessary to programmatically
-// create pages.
-// Will create pages for WordPress pages (route : /{slug})
-// Will create pages for WordPress posts (route : /post/{slug})
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  return new Promise((resolve, reject) => {
-    // The “graphql” function allows us to run arbitrary
-    // queries against the local WordPress graphql schema. Think of
-    // it like the site has a built-in database constructed
-    // from the fetched data that you can run queries against.
-
-    // ==== BLOG POSTS ====
-    graphql(`
-      {
-        wpgraphql {
-          posts {
-            edges {
-              node {
-                excerpt
-                postId
-                date
-                title
-                content
-                slug
-              }
-            }
-          }
-        }
-      }
-    `).then(result => {
-      if (result.errors) {
-        console.log(result.errors)
-        reject(result.errors)
-      }
-
-      const posts = result.wpgraphql.posts.edges
-      const postsPerPage = 3
-      const numberOfPages = Math.ceil(posts.length / postsPerPage)
-      const blogPostListTemplate = path.resolve(
-        "./src/templates/blogPostList.js"
-      )
-
-      Array.from({ length: numberOfPages }).forEach((page, index) => {
-        createPage({
-          component: slash(blogPostListTemplate),
-          path: index === 0 ? "/blog" : `/blog/${index + 1}`,
-          context: {
-            posts: posts.slice(
-              index * postsPerPage,
-              index * postsPerPage + postsPerPage
-            ),
-            numberOfPages,
-            currentPage: index + 1,
-          },
-        })
-      })
-
-      const pageTemplate = path.resolve("./src/templates/blogpost.js")
-      _.each(posts, post => {
-        createPage({
-          path: `${post.node.slug}`,
-          component: slash(pageTemplate),
-          context: post.node,
-        })
-      })
-
-      resolve()
-    })
-  })
+exports.createPagesStatefully = async (
+  { graphql, actions, reporter },
+  options
+) => {
+  await createPosts({ actions, graphql, reporter }, options)
 }
 
 /* create remotefile */
