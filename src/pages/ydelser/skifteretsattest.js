544cf668a831f615cb34dc6552d4aafc3e02679f
TurboTobiasDK <mig@turbotobias.dk>
2020-07-09 10:17:56 +0200

    update packages

--- src/pages/ydelser/skifteretsattest.js
+++ src/pages/ydelser/skifteretsattest.js
@@ -83,6 +83,7 @@ Indtast din kontaktoplysninger nedenfor:</p>
               className="hero-form"
               data-netlify-honeypot="bot-field"
             >
+              <input type="hidden" name="subject" value="Bestil skifteretsattest" />
               <input type="hidden" name="form-name" value="contact" />
               <input type="hidden" name="bot-field" />
               <p>
