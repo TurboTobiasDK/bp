9b664993d20e7e94d49ba7e4d94f4976e71daeee
TurboTobiasDK <mig@turbotobias.dk>
2020-07-12 20:39:50 +0200

    update hero img loading

--- src/pages/forretningsbetingelser.js
+++ src/pages/forretningsbetingelser.js
@@ -18,7 +18,7 @@ const forretningsbetingelserPage = props => {
       <section className="hero">
         <div className="hero__image-wrapper">
           <Img
-            fadeIn="false"
+            fadeIn={false}
             loading="eager"
             fluid={
               wpgraphql.pages.edges[0].node.politikACFgraphql.hero.heroImage
