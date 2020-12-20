12c3c6e76b1434c64506b0a5f437a4b6637d7164
TurboTobiasDK <mig@turbotobias.dk>
2020-12-20 13:20:50 +0100

    fix featured img

--- src/components/FluidImage.js
+++ src/components/FluidImage.js
@@ -2,28 +2,35 @@ import React from "react"
 import GatsbyImage from "gatsby-image"
 import { graphql, useStaticQuery } from "gatsby"
 
-
 const FluidImage = ({ image, withFallback = false, ...props }) => {
   const data = useStaticQuery(graphql`
-      query {
-          fallbackImage: file(relativePath: { eq: "fallback.svg" }) {
-              publicURL
-          }
+    query {
+      fallbackImage: file(relativePath: { eq: "fallback.svg" }) {
+        publicURL
       }
+    }
   `)
 
   /**
    * Return fallback Image, if no Image is given.
    */
   if (!image) {
-    return withFallback ? <img src={data.fallBackImage.publicURL} alt={"Fallback"} {...props}/> : null
+    return withFallback ? (
+      <img src={data.fallBackImage.publicURL} alt={"Fallback"} {...props} />
+    ) : null
   }
 
   if (image && image.imageFile) {
-    return <GatsbyImage fluid={image.imageFile.childImageSharp.fluid} alt={image.altText} {...props}/>
+    return (
+      <GatsbyImage
+        fluid={image.node.imageFile.childImageSharp.fluid}
+        alt={image.node.altText}
+        {...props}
+      />
+    )
   }
 
-  return <img src={image.sourceUrl} alt={image.altText} {...props}/>
+  return <img src={image.node.sourceUrl} alt={image.node.altText} {...props} />
 }
 
 export default FluidImage
